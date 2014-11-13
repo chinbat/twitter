3c6e075b59b5fc2a3b479834488ff895e0784dfe
Chinbaa <chinbaa.chin@gmail.com>
2014-11-13 16:56:47 +0900

    img edit

--- learning_ruby/img_test.rb
+++ learning_ruby/img_test.rb
@@ -1,5 +1,20 @@
 require 'rubygems'
 require 'chunky_png'
+require 'set'
 
-image = ChunkyPNG::Image.from_file('test.png')
-puts ChunkyPNG::Color.to_truecolor_bytes(image[300,400])
+image = ChunkyPNG::Image.from_file('weather.png')
+colors = Set.new
+cnt = 0
+for x in 0..255
+  for y in 0..255
+    if ChunkyPNG::Color.to_truecolor_bytes(image[x,y]) != [0,0,0]
+      colors.add(ChunkyPNG::Color.to_truecolor_bytes(image[x,y]))
+    end
+  end
+end
+
+colors.each do |color|
+puts color
+puts
+end
+
