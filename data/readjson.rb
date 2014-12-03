714d2de79b817c887baf15dccddccf11bbfb987c
Chinbaa <chinbaa.chin@gmail.com>
2014-12-04 07:00:39 +0900

    added coordinates

--- data/readjson.rb
+++ data/readjson.rb
@@ -4,14 +4,13 @@ start = Time.now
 
 json = File.read('twout3_edited.json')
 data_array = JSON.parse(json)["tweets"]
-out = File.open('rain3.txt','w')
+out = File.open('coordinates.txt','w')
 cnt = 0
 data_array.each do |tweet|
-  if tweet["rain"] != 0
-    cnt += 1
-    out.puts tweet["created_at"],tweet["text"],tweet["coordinates"],tweet["place"]["full_name"]
-    out.puts
-  end
+  coordinates = tweet["coordinates"]
+  lat = coordinates[/(?<=\[).+(?=\,)/].to_f
+  long = coordinates[/(?<=\s).+(?=\])/].to_f
+  out.puts "#{lat},#{long}"
 end
 
 puts cnt
