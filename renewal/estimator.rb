e94dffa04b09eeacf8ac381ef1d9b6002bbd7599
chinbaa <chinbaa@localhost.localdomain>
2015-01-12 09:15:10 +0900

    estimator executed

--- renewal/estimator.rb
+++ renewal/estimator.rb
@@ -1,18 +1,32 @@
 require "json"
 require "fileutils"
 
+
+R = 6371
+def distance(lat1,lon1,lat2,lon2)
+  dlat = deg2rad(lat2-lat1)
+  dlon = deg2rad(lon2-lon1)
+  a = Math.sin(dlat/2) * Math.sin(dlat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dlon/2) * Math.sin(dlon/2)
+  c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+  d = R * c
+  return d
+end
+
+def deg2rad(deg)
+  return deg * (Math::PI/180)
+end
+
 #begin
   #$stdout.reopen("stdout.txt","a")
   #$stderr.reopen("stderr.txt","a")
 
-  log = File.open("../../data/estimator_log.txt","w")
+  log = File.open("../../data/estimator_log.txt","a")
   valid_users = Array.new
   valid = File.foreach("../../data/valid_users")
   valid.each do |user|
     valid_users << user.to_i
   end
   
-  #res = File.open("estimation_res.txt","a")
   uc = 0
   aw = File.read("../../data/o50_json.txt")
   allw = JSON.parse(aw)["words"]
@@ -21,17 +35,19 @@ require "fileutils"
     all_words[key] = value
   end
   corpus = 20179133
-  
-  valid_users.each do |user|
+  #for i in 0..valid_users.length-1
+  for i in 0..5000
     t1 = Time.now
+    user = valid_users[i]
     uc += 1
-    #if uc!=2
-      #next
-    #end
+    res = File.open("../../data/est_res/#{user}.txt","w")
     coordinates = Hash.new(0)
     file = File.read("../../data/word_user/#{user}.json")
     data = JSON.parse(file)
     rloc = data["user"]["rloc"]
+    rloc_sp = rloc.split(',')
+    rlat = rloc_sp[0].to_f
+    rlon = rloc_sp[1].to_f
     data["words"].each do |key,value|
       fn = "../../data/gois/#{key}"
       if File.exist?(fn)
@@ -42,22 +58,36 @@ require "fileutils"
           long = t[1].to_f
           n = t[2].to_f
           coordinate = "#{lat},#{long}"
-          #puts all_words["ピル"]
           coordinates[coordinate] += value.to_f * n / corpus.to_f
-          #something
+          # new model below
         end
       end
     end
     coordinates = coordinates.sort_by{|key,value| value}.reverse
-    log.puts "#{user}: #{rloc}"
+    #log.puts "#{user}: #{rloc}"
+    cr_cnt = 0
+    num = 0
+    first_num = 0
+    flag = true
     coordinates.each do |key,value|
-      log.puts "#{key},#{value}"
+      cr_cnt += 1
+      if key == rloc
+        num = cr_cnt
+      end
+      t = key.split(',')
+      lat = t[0].to_f
+      lon = t[1].to_f
+      dist = distance(rlat,rlon,lat,lon)
+      if flag and dist <= 160
+        first_num = cr_cnt
+        flag = false
+      end
+      res.puts "#{key},#{value}"
     end
     t2 = Time.now
-    log.puts "Time: #{t2-t1}"
-    if uc == 1
-      exit
-    end
+    log.puts "#{uc},#{user},#{rloc},#{first_num},#{num},#{coordinates.length},#{coordinates[0][1]},#{t2-t1}"
+    log.close
+    log = File.open("../../data/estimator_log.txt","a")
   end
 
 #rescue Exception => e
