e22d5d7fa332153dcc3708cbb5955ebe95d24f8e
chinbaa <chinbaa@localhost.localdomain>
2015-01-14 10:16:10 +0900

    valid_user list creator, new model

--- renewal/valid_user.rb
+++ renewal/valid_user.rb
@@ -20,35 +20,58 @@ end
   #$stdout.reopen("stdout.txt","a")
   #$stderr.reopen("stderr.txt","a")
 
-  log = File.open("../../data/estimator_log_2.txt","a")
+  log = File.open("../../data/valid_user_log.txt","w")
   valid_users = Array.new
   valid = File.foreach("../../data/valid_users")
   valid.each do |user|
     valid_users << user.to_i
   end
+  res = File.open("../../data/new_valid_user","w")
   
   uc = 0
-  aw = File.read("../../data/o50_json.txt")
-  allw = JSON.parse(aw)["words"]
-  all_words = Hash.new
-  allw.each do |key,value|
-    all_words[key] = value
-  end
-  corpus = 20179133
-  #for i in 0..valid_users.length-1
-  for i in 0..5000
-    t1 = Time.now
+  #aw = File.read("../../data/o50_json.txt")
+  #allw = JSON.parse(aw)["words"]
+  #all_words = Hash.new
+  #allw.each do |key,value|
+    #all_words[key] = value
+  #end
+  #corpus = 20179133
+  for i in 0..valid_users.length-1
+  #for i in 0..5000
+    #t1 = Time.now
     user = valid_users[i]
-    uc += 1
-    res = File.open("../../data/est_res_2/#{user}.txt","w")
-    coordinates = Hash.new(0)
+    #uc += 1
+    #res = File.open("../../data/est_res_2/#{user}.txt","w")
+    #coordinates = Hash.new(0)
     file = File.read("../../data/word_user/#{user}.json")
     data = JSON.parse(file)
     rloc = data["user"]["rloc"]
     rloc_sp = rloc.split(',')
     rlat = rloc_sp[0].to_f
     rlon = rloc_sp[1].to_f
-    u_corpus = 0
+    valid = false
+    if rlon >= 123.75 and rlon <= 129.375 and rlat >= 21.94 and rlat <= 31.95
+      valid = true
+    elsif rlon >= 129.375 and rlon <= 135 and rlat >= 27.05 and rlat <= 36.59
+      valid = true
+    elsif rlon >= 135 and rlon <= 146.25 and rlat >= 31.95 and rlat <= 36.59
+      valid = true
+    elsif rlon >= 135 and rlon <= 146.25 and rlat >= 36.59 and rlat <= 40.98
+      valid = true
+    elsif rlon >= 135 and rlon <= 146.25 and rlat >= 40.98 and rlat <= 45.08
+      valid = true
+    end
+    #if i == 16
+    #  puts rlon
+    #  puts valid
+    #  exit
+    #end
+    if valid
+      uc += 1
+      res.puts user
+    end
+    #u_corpus = 0
+=begin
     data["words"].each do |key,value|
       fn = "../../data/gois/#{key}"
       if File.exist?(fn)
@@ -71,7 +94,7 @@ end
       end
     end
     coordinates = coordinates.sort_by{|key,value| value}.reverse
-    #log.puts "#{user}: #{rloc}"
+    log.puts "#{user}: #{rloc}"
     cr_cnt = 0
     num = 0
     first_num = 0
@@ -95,8 +118,9 @@ end
     log.puts "#{uc},#{user},#{rloc},#{first_num},#{num},#{coordinates.length},#{coordinates[0][1]},#{t2-t1}"
     log.close
     log = File.open("../../data/estimator_log_2.txt","a")
+=end
   end
-
+  log.puts uc,valid_users.length
 #rescue Exception => e
 #  log.puts e.message
 #end
